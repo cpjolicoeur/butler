7c2c77e9546fa784aa526511162abf7d85c9939d
Craig P Jolicoeur <cpjolicoeur@gmail.com>
2008-12-18 12:36:00 -0500

    use views directory instead of in file templates

--- butler.rb
+++ butler.rb
@@ -1,43 +1,16 @@
-#! /usr/bin/env ruby
+require 'rubygems'
 require 'sinatra'
 require 'pathname'
 
 get "/" do
   dir = "./files/"
-  @links = Dir[dir+"*"].map { |file| 
-    file_link(file)
-  }.join
+  @links = Dir[dir+"*"].map { |file| file_link( file ) }.join
   erb :index
 end
 
 helpers do
-
-  def file_link(file)
-    filename = Pathname.new(file).basename
-    "<li><a href='#{file}' target='_self'>#{filename}</a></li>"
+  def file_link( _file )
+    filename = Pathname.new( _file ).basename
+    "<li><a href='#{_file}' target='_self'>#{filename}</a></li>"
   end
-  
 end
-
-use_in_file_templates!
-
-__END__
-
-@@ index
-<html>
-  <head>
-    <meta name="viewport" content="width=320; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;"/>
-    <style type="text/css" media="screen">@import "/stylesheets/iui.css";</style>
-    <script type="application/x-javascript" src="/javascripts/iui.js"></script>
-  </head>
-
-  <body>
-    <div class="toolbar">
-    <h1 id="pageTitle"></h1>
-    </div>
-    <ul id="home" title="Your files, sir." selected="true">
-       <%= @links %>
-    </ul>
-  </body>
-
-</html>
